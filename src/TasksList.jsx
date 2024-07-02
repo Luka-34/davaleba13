603b0ee81ed62afb8779060499bd385bb8d9a4dd
Luka-34 <lchikhladze960@gmail.com>
2024-07-02 13:45:57 +0400

    intial commit

--- src/TasksList.jsx
+++ src/TasksList.jsx
@@ -1,91 +1,81 @@
-import { PureComponent } from "react";
+import React, { useState } from "react";
 
-class TasksList extends PureComponent {
-    
-    state = { 
-        inputValue: '',
-        description: [
-            {toDo: "Task1", id: 1, completed: false}, 
-            {toDo: "Task2", id: 2, completed: false}
-        ]
-    } 
+const TasksList = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [description, setDescription] = useState([
+        { toDo: "Task1", id: 1, completed: false },
+        { toDo: "Task2", id: 2, completed: false }
+    ]);
 
-    onChange = (e) => {
-        const value = e.target.value;
-        this.setState({
-            inputValue: value
-        });
+    const onChange = (e) => {
+        setInputValue(e.target.value);
     }
 
-    addTask = (e) => {
+    const addTask = (e) => {
         e.preventDefault();
 
         const newTask = {
-            id: this.state.description.length + 1,
-            toDo: this.state.inputValue,
+            id: description.length + 1,
+            toDo: inputValue,
             completed: false
         };
 
-        this.setState(prevState => ({
-            description: [...prevState.description, newTask],
-            inputValue: ''
-        }));
+        setDescription(prevDescription => [...prevDescription, newTask]);
+        setInputValue('');
     }
 
-    toggleComplete = (id) => {
-        this.setState(prevState => ({
-            description: prevState.description.map(task =>
+    const toggleComplete = (id) => {
+        setDescription(prevDescription =>
+            prevDescription.map(task =>
                 task.id === id ? { ...task, completed: !task.completed } : task
             )
-        }));
-    }
-
-    deleteTask = (id) => {
-        this.setState(prevState => ({
-            description: prevState.description.filter(task => task.id !== id)
-        }));
-    }
-
-    render() {
-        return (
-            <div className="cont">
-                <div className="title">
-                    <h1>ToDo List</h1>
-                </div>
-                <form onSubmit={this.addTask}>
-                    <input 
-                        type="text" 
-                        className="add-input" 
-                        value={this.state.inputValue} 
-                        onChange={this.onChange}
-                    />
-                    <button type="submit" className="add">Add</button>
-                </form>
-                <div className="cont2">
-                    <div className="toDO">
-                        <h1 className="heading">ToDo</h1>
-                        {this.state.description.filter(task => !task.completed).map((descr) => (
-                            <div key={descr.id}>
-                                <p>{descr.toDo}</p>
-                                <button onClick={() => this.toggleComplete(descr.id)}>Complete</button>
-                                <button onClick={() => this.deleteTask(descr.id)}>Delete</button>
-                            </div>
-                        ))}
-                    </div>
-                    <div className="done">
-                        <h1 className="heading">Done</h1>
-                        {this.state.description.filter(task => task.completed).map((descr) => (
-                            <div key={descr.id}>
-                                <p>{descr.toDo}</p>
-                                <button onClick={() => this.toggleComplete(descr.id)}>Undo</button>
-                                <button onClick={() => this.deleteTask(descr.id)}>Delete</button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
         );
     }
+
+    const deleteTask = (id) => {
+        setDescription(prevDescription =>
+            prevDescription.filter(task => task.id !== id)
+        );
+    }
+
+    return (
+        <div className="cont">
+            <div className="title">
+                <h1>ToDo List</h1>
+            </div>
+            <form onSubmit={addTask}>
+                <input 
+                    type="text" 
+                    className="add-input" 
+                    value={inputValue} 
+                    onChange={onChange}
+                />
+                <button type="submit" className="add">Add</button>
+            </form>
+            <div className="cont2">
+                <div className="toDO">
+                    <h1 className="heading">ToDo</h1>
+                    {description.filter(task => !task.completed).map((descr) => (
+                        <div key={descr.id}>
+                            <p>{descr.toDo}</p>
+                            <button onClick={() => toggleComplete(descr.id)}>Complete</button>
+                            <button onClick={() => deleteTask(descr.id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+                <div className="done">
+                    <h1 className="heading">Done</h1>
+                    {description.filter(task => task.completed).map((descr) => (
+                        <div key={descr.id}>
+                            <p>{descr.toDo}</p>
+                            <button onClick={() => toggleComplete(descr.id)}>Undo</button>
+                            <button onClick={() => deleteTask(descr.id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default TasksList;
